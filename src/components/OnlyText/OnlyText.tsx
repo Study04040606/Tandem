0d44d7626094cbd436ea24eb0be52c4f9f64a4ef
Study04040606 <study04040606@gmail.com>
2026-01-04 18:56:39 +0100

    add onlyText component

---
+++ src/components/OnlyText/OnlyText.tsx
@@ -0,0 +1,30 @@
+
+import Container from "@/components/Container/Container.tsx";
+import Button from "@/components/Button/Button";
+
+type OnlyTextProps = {
+    title: string;
+    text: Array<string>;
+}   
+
+export default function OnlyText({title, text}: OnlyTextProps) {
+    return (
+        <section className="bg-neutral-200 ">
+            <Container>
+                <div className="flex flex-col p-4 lg:p-8 lg:m-8">
+                    <h2 className='text-2xl font-bold text-primary mb-4'>
+                          {title}
+                    </h2>
+
+                    {text.map((paragraph, index) => (
+                        <p key={index} className="mb-4">
+                            {paragraph}
+                        </p>
+                    ))}
+
+                    <Button />
+                </div>
+            </Container>
+        </section>        
+    )
+}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,20 @@
+import { useTranslation } from "react-i18next";
 import Layout from '@/components/Layout/Layout.tsx';
 import Hero from '@/components/Hero/Hero.tsx';
 import AboutUs from '@/components/AboutUs/AboutUs.tsx';
 import ForWhom from '@/components/ForWhom/ForWhom.tsx';
+import OnlyText from '@/components/OnlyText/OnlyText';
 
 const HomePage = () => {
+     const { t } = useTranslation();
 
 return (
     <Layout>
         <Hero />
         <AboutUs />
         <ForWhom />
+        <OnlyText title={t('what-you-get-title')} text={[t('what-you-get-paragraph-1'), t('what-you-get-paragraph-2'), t('what-you-get-paragraph-3'), t('what-you-get-paragraph-4')]}/>
+        <OnlyText title={t('idea-title')} text={[t('idea-paragraph-1'), t('idea-paragraph-2'), t('idea-paragraph-3')]}/>
     </Layout>
 );
 };
